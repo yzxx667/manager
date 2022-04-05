b613761cc87604d09faa1637c3cc9b5c4e874d7c
尹志祥 <13602547976@163.com>
2022-04-05 23:43:40 +0800

    request1

---
+++ src/utils/request.js
@@ -0,0 +1,49 @@
+// axios二次封装
+import axios from "axios"
+import config from "../config"
+import { ElMessage } from 'element-plus' 
+import router from './../router/index.js'
+const TOKEN_INVALID = 'Token认证失败，请重新登录'
+const NETWORK_ERROR = '网络请求异常，请稍后重试'
+
+// 创建axios实例对象，添加全局配置 
+const service = axios.create({
+    baseURL: config.baseApi,
+    timeout: 8000
+})
+
+// 请求拦截
+service.interceptors.request.use((req)=>{
+    // TO_DO
+
+    // token
+    const headers = req.headers;
+    if( !headers.Authorization) headers.Authorization = 'Bear Jack'
+    return req
+})
+
+// 响应拦截
+service.interceptors.response.use((res)=>{
+    const { code, data, msg} = res.data;
+    if(code === 200){
+        return data
+    }else if(code === 40001){
+        ElMessage.error(TOKEN_INVALID)
+        setTimeout(() => {
+            router.push('/login')
+        }, 1500);
+        return Promise.reject(TOKEN_INVALID)
+    }else{
+        ElMessage.error(msg || NETWORK_ERROR)
+        return Promise.reject(msg || NETWORK_ERROR)
+    }
+})
+
+//请求的核心函数
+function request(options){
+    return service(options)
+}
+
+export default {
+    request
+}
